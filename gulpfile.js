57baade1d253e4a41d7512e968a13362eb531891
snpr <vaugk13@gmail.com>
2019-08-06 17:19:18 +0500

    Add task for the build directory deletion

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var del = require("del");
 
 gulp.task("css", function() {
   return gulp
@@ -84,6 +85,10 @@ gulp.task("copy", function() {
       }
     )
     .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
 });
 
 gulp.task("server", function() {
