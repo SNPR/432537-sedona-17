114666426827f4fd8dc599bce2cb8e90dc810173
snpr <vaugk13@gmail.com>
2019-08-07 12:35:36 +0500

    Create folder for the sprites; Change path in the sprite task and watcher

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ gulp.task("webp", function() {
 
 gulp.task("sprite", function() {
   return gulp
-    .src("source/img/icon-video*.svg")
+    .src("source/img/sprites/icon-*.svg")
     .pipe(
       svgstore({
         inlineSvg: true
@@ -100,7 +100,7 @@ gulp.task("server", function() {
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch(
-    "source/img/icon-video*.svg",
+    "source/img/spries/icon-*.svg",
     gulp.series("sprite", "html", "refresh")
   );
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
