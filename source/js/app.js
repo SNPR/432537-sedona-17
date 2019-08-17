d60f88b960500791e5355445988b57ff713ecb16
snpr <vaugk13@gmail.com>
2019-08-17 11:54:36 +0500

    Hide navigation list only for mobile viewport

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,19 @@
 var navigationList = document.querySelector(".navigation__list");
 var navigationControl = document.querySelector(".navigation__control-button");
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
-navigationControl.addEventListener("click", function() {
-  navigationControl.classList.toggle("navigation__control-button--hamburger");
-  navigationList.classList.toggle("navigation__list--hidden");
-});
+if (viewportWidth < 768) {
+  if (navigationControl) {
+    navigationControl.classList.remove("navigation__control-button--nojs");
+    navigationControl.classList.add("navigation__control-button--hamburger");
+  }
+
+  if (navigationList) {
+    navigationList.classList.add("navigation__list--hidden");
+  }
+
+  navigationControl.addEventListener("click", function() {
+    navigationControl.classList.toggle("navigation__control-button--hamburger");
+    navigationList.classList.toggle("navigation__list--hidden");
+  });
+}
